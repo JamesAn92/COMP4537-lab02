147eaa0f9ec374c0c00f33ea933c31a760507d39
james <jan31@my.bcit.ca>
2023-01-12 10:33:26 -0800

    rep Setup

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+
+var cors = require('cors');
+
+const app = express();
+app.use(cors());
+const PORT = 3000 || process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+app.use(bodyParser.json()); ///// prob here apparently
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/chatbot', (req, res) => {
+	const message = req.body.message;
+
+    
+	const number = message.match(42);
+	if (number) {
+		fetch(`http://numbersapi.com/${number}?type=trivia`).then(response => response.text()).then(data => {
+			res.json({
+				text: data
+			});
+		}).catch(error => {
+			res.json({
+				text: "Sorry, I couldn't find any information about that number."
+			});
+		});
+	} else {
+		res.json({
+			text: "I'm sorry, I didn't understand your question. Please provide a number for me to give you information about."
+		});
+	}
+});
+
+// app.get('/', (req, res) => {
+//     res.send()
+// })
+
+// const PORT = 3000;
+// app.listen(PORT, () => {
+// 	console.log(`Server is running on port ${PORT}`);
+// });
