ac0d44a6d6abd5edeb785073aa675cfe21f2fd81
james <jan31@my.bcit.ca>
2023-01-18 10:33:11 -0800

    added node-version

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 const path = require('path');
 var cors = require('cors');
 const app = express();
+//const fetch = require("node-fetch");
 
 app.use(cors());
 const PORT = 3000 || process.env.PORT;
@@ -18,7 +19,8 @@ app.post('/', (req, res) => {
 	const message = req.body.message;    
 	const number = message.match(/\d+/);
 	if (number) {
-		fetch(`http://numbersapi.com/${number}?type=trivia`).then(response => response.text()).then(data => {
+		(async () => {
+			await fetch(`http://numbersapi.com/${number}?type=trivia`).then(response => response.text()).then(data => {
 			res.json({
 				text: data
 			});
@@ -27,6 +29,8 @@ app.post('/', (req, res) => {
 				text: "Sorry, I couldn't find any information about that number."
 			});
 		});
+		})
+		
 	} else {
 		res.json({
 			text: "I'm sorry, I didn't understand your question. Please provide a number for me to give you information about."
