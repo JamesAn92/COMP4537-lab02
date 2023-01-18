56458f794a71c3f6fc9e55ccf55d3ac9a089d2cb
james <jan31@my.bcit.ca>
2023-01-18 02:37:48 -0800

    debugging

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ const multer = require('multer');
 const path = require('path');
 var cors = require('cors');
 const app = express();
-const fetch = require('node-fetch');
 
 app.use(cors());
 const PORT = 3000 || process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-app.use(bodyParser.json()); ///// prob here apparently
+app.use(bodyParser.json()); ///// prob here 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/chatbot', (req, res) => {
-	const message = req.body.message;
+app.post('/', (req, res) => {
 
-    
+	const message = req.body.message;    
 	const number = message.match(/\d+/);
 	if (number) {
 		fetch(`http://numbersapi.com/${number}?type=trivia`).then(response => response.text()).then(data => {
