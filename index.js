330b90d29bdcc985c1356ecb5a55e51db5da09c1
james <jan31@my.bcit.ca>
2023-01-18 22:22:50 -0800

    Fixed styles and js link issue using express routes

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const multer = require('multer');
 const path = require('path');
 var cors = require('cors');
 const app = express();
-//const fetch = require("node-fetch");
 
 app.use(cors());
+
+// set port to 3000 and listen
 const PORT = 3000 || process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-app.use(bodyParser.json()); ///// prob here 
+
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/', (req, res) => {
-
+	res.set('Content-Type', 'application/javascript');
 	const message = req.body.message;    
 	const number = message.match(/\d+/);
 	if (number) {
@@ -38,35 +40,17 @@ app.post('/', (req, res) => {
 	}
 });
 
-// app.get('/chatbot', (req, res) => {
-// 	const message = req.body.message;
+app.get('/script.js', function (req, res) {
+	res.type('application/javascript');
+	res.sendFile(__dirname + '/script.js');
+});
 
-    
-// 	const number = message.match(42);
-// 	if (number) {
-// 		fetch(`http://numbersapi.com/${number}?type=trivia`).then(response => response.text()).then(data => {
-// 			res.json({
-// 				text: data
-// 			});
-// 		}).catch(error => {
-// 			res.json({
-// 				text: "Sorry, I couldn't find any information about that number."
-// 			});
-// 		});
-// 	} else {
-// 		res.json({
-// 			text: "I'm sorry, I didn't understand your question. Please provide a number for me to give you information about."
-// 		});
-// 	}
-// });
-
+app.get('/styles.css', function (req, res) {
+	res.type('text/css');
+	res.sendFile(__dirname + '/styles.css');
+});
 
 app.get('/', (req, res) => {
 	res.set('Content-Type', 'text/html');
 	res.sendFile(path.join(__dirname, '/index.html'));
 });
-
-// const PORT = 3000;
-// app.listen(PORT, () => {
-// 	console.log(`Server is running on port ${PORT}`);
-// });
