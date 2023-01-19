9bd6a1c1919297ab83c92ec5dbe7d39b4aa14b4d
james <jan31@my.bcit.ca>
2023-01-18 22:30:49 -0800

    Bug fix

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/chatbot', (req, res) => {
-	res.set('Content-Type', 'application/javascript');
 	const message = req.body.message;    
 	const number = message.match(/\d+/);
 	if (number) {
